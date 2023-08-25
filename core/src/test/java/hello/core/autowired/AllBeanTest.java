f663e8ce0cee564360266c3b0111bd13b8d07872
pysdsc <pysdsc@gmail.com>
2023-08-26 00:04:39 +0900

    9. 빈 스코프

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -15,7 +15,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 
 public class AllBeanTest {
     @Test
